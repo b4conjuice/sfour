20251f5f691c960dd2f53c9e6b694ea69366e945
Daniel Lopez <d@niel.to>
2026-04-22 16:09:08 -0700

    update homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,10 @@ export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
-    <div className='p-8'>
-      <h1 className='text-4xl font-bold'>Welcome to TanStack Start</h1>
-      <p className='mt-4 text-lg'>
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
-    </div>
+    <main className='flex grow flex-col p-4'>
+      <div className='flex grow flex-col items-center justify-center space-y-4'>
+        <h1 className='font-bold'>📖</h1>
+      </div>
+    </main>
   )
 }
